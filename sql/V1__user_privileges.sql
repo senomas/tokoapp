c0be612de40c3e6ac91f8639e6c28f93cee51165
denmaseno <agus@hanoman.co.id>
2022-02-08 14:59:15 +0700

    fix policy

--- sql/V1__user_privileges.sql
+++ sql/V1__user_privileges.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS roles CASCADE;
-DROP TABLE IF EXISTS role_privileges CASCADE;
-DROP TABLE IF EXISTS privileges CASCADE;
-DROP TABLE IF EXISTS user_roles CASCADE;
+DROP TABLE IF EXISTS app_roles CASCADE;
+DROP TABLE IF EXISTS app_role_permissions CASCADE;
+DROP TABLE IF EXISTS app_permissions CASCADE;
+DROP TABLE IF EXISTS user_app_roles CASCADE;
 
-CREATE TABLE roles (
+CREATE TABLE app_roles (
   id SERIAL NOT NULL PRIMARY KEY,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   created_by UUID REFERENCES auth.users,
@@ -13,13 +13,13 @@ CREATE TABLE roles (
   description TEXT
 );
 
-ALTER TABLE roles ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "allow select roles to all users" ON public.roles FOR SELECT USING (true);
-CREATE POLICY "allow update roles to users based on email" ON public.roles FOR UPDATE USING (auth.email() LIKE '%@senomas.com') WITH CHECK (auth.email() = 'agus@senomas.com');
-CREATE POLICY "allow insert roles to users based on email" ON public.roles FOR INSERT WITH CHECK (auth.email() LIKE '%@senomas.com');
-CREATE POLICY "allow delete roles to users based on email" ON public.roles FOR DELETE USING (auth.email() LIKE '%@senomas.com');
+ALTER TABLE app_roles ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "allow select app_roles to all users" ON public.app_roles FOR SELECT USING (true);
+CREATE POLICY "allow update app_roles to users based on email" ON public.app_roles FOR UPDATE USING (auth.email() LIKE '%@senomas.com') WITH CHECK (auth.email() = 'agus@senomas.com');
+CREATE POLICY "allow insert app_roles to users based on email" ON public.app_roles FOR INSERT WITH CHECK (auth.email() LIKE '%@senomas.com');
+CREATE POLICY "allow delete app_roles to users based on email" ON public.app_roles FOR DELETE USING (auth.email() LIKE '%@senomas.com');
 
-CREATE TABLE privileges (
+CREATE TABLE app_permissions (
   id SERIAL NOT NULL PRIMARY KEY,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   created_by UUID REFERENCES auth.users,
@@ -29,57 +29,59 @@ CREATE TABLE privileges (
   description TEXT
 );
 
-ALTER TABLE privileges ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "allow select privileges to all users" ON public.privileges FOR SELECT USING (true);
-CREATE POLICY "allow update privileges to users based on email" ON public.privileges FOR UPDATE USING (auth.email() LIKE '%@senomas.com') WITH CHECK (auth.email() = 'agus@senomas.com');
-CREATE POLICY "allow insert privileges to users based on email" ON public.privileges FOR INSERT WITH CHECK (auth.email() LIKE '%@senomas.com');
-CREATE POLICY "allow delete privileges to users based on email" ON public.privileges FOR DELETE USING (auth.email() LIKE '%@senomas.com');
+ALTER TABLE app_permissions ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "allow select app_permissions to all users" ON public.app_permissions FOR SELECT USING (true);
+CREATE POLICY "allow update app_permissions to users based on email" ON public.app_permissions FOR UPDATE USING (auth.email() LIKE '%@senomas.com') WITH CHECK (auth.email() = 'agus@senomas.com');
+CREATE POLICY "allow insert app_permissions to users based on email" ON public.app_permissions FOR INSERT WITH CHECK (auth.email() LIKE '%@senomas.com');
+CREATE POLICY "allow delete app_permissions to users based on email" ON public.app_permissions FOR DELETE USING (auth.email() LIKE '%@senomas.com');
 
 
-CREATE TABLE user_roles (
+CREATE TABLE user_app_roles (
   id SERIAL NOT NULL PRIMARY KEY,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   created_by UUID REFERENCES auth.users,
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   updated_by UUID REFERENCES auth.users,
   user_id UUID REFERENCES auth.users,
-  role_id INT REFERENCES roles,
-  CONSTRAINT user_roles_link UNIQUE(user_id, role_id)
+  app_role_id INT REFERENCES app_roles,
+  CONSTRAINT user_app_roles_link UNIQUE(user_id, app_role_id)
 );
 
-ALTER TABLE user_roles ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "allow select user_roles to all users" ON public.user_roles FOR SELECT USING (true);
-CREATE POLICY "allow update user_roles to users based on email" ON public.user_roles FOR UPDATE USING (auth.email() LIKE '%@senomas.com') WITH CHECK (auth.email() = 'agus@senomas.com');
-CREATE POLICY "allow insert user_roles to users based on email" ON public.user_roles FOR INSERT WITH CHECK (auth.email() LIKE '%@senomas.com');
-CREATE POLICY "allow delete user_roles to users based on email" ON public.user_roles FOR DELETE USING (auth.email() LIKE '%@senomas.com');
+ALTER TABLE user_app_roles ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "allow select user_app_roles to all users" ON public.user_app_roles FOR SELECT USING (true);
+CREATE POLICY "allow update user_app_roles to users based on email" ON public.user_app_roles FOR UPDATE USING (auth.email() LIKE '%@senomas.com') WITH CHECK (auth.email() = 'agus@senomas.com');
+CREATE POLICY "allow insert user_app_roles to users based on email" ON public.user_app_roles FOR INSERT WITH CHECK (auth.email() LIKE '%@senomas.com');
+CREATE POLICY "allow delete user_app_roles to users based on email" ON public.user_app_roles FOR DELETE USING (auth.email() LIKE '%@senomas.com');
 
 
-CREATE TABLE role_privileges (
+CREATE TABLE app_role_permissions (
   id SERIAL NOT NULL PRIMARY KEY,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   created_by UUID REFERENCES auth.users,
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   updated_by UUID REFERENCES auth.users,
-  role_id INT REFERENCES roles,
-  privilege_id INT REFERENCES privileges,
-  CONSTRAINT role_privileges_link UNIQUE(role_id, privilege_id)
+  app_role_id INT REFERENCES app_roles,
+  app_permission_id INT REFERENCES app_permissions,
+  CONSTRAINT app_role_permissions_link UNIQUE(app_role_id, app_permission_id)
 );
 
-ALTER TABLE role_privileges ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "allow select role_privileges to all users" ON public.role_privileges FOR SELECT USING (true);
-CREATE POLICY "allow update role_privileges to users based on email" ON public.role_privileges FOR UPDATE USING (auth.email() LIKE '%@senomas.com') WITH CHECK (auth.email() = 'agus@senomas.com');
-CREATE POLICY "allow insert role_privileges to users based on email" ON public.role_privileges FOR INSERT WITH CHECK (auth.email() LIKE '%@senomas.com');
-CREATE POLICY "allow delete role_privileges to users based on email" ON public.role_privileges FOR DELETE USING (auth.email() LIKE '%@senomas.com');
+ALTER TABLE app_role_permissions ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "allow select app_role_permissions to all users" ON public.app_role_permissions FOR SELECT USING (true);
+CREATE POLICY "allow update app_role_permissions to users based on email" ON public.app_role_permissions FOR UPDATE USING (auth.email() LIKE '%@senomas.com') WITH CHECK (auth.email() = 'agus@senomas.com');
+CREATE POLICY "allow insert app_role_permissions to users based on email" ON public.app_role_permissions FOR INSERT WITH CHECK (auth.email() LIKE '%@senomas.com');
+CREATE POLICY "allow delete app_role_permissions to users based on email" ON public.app_role_permissions FOR DELETE USING (auth.email() LIKE '%@senomas.com');
 
-INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO app_roles (name) VALUES ('admin');
 
-INSERT INTO privileges (name) VALUES ('items.update');
-INSERT INTO privileges (name) VALUES ('items.insert');
-INSERT INTO privileges (name) VALUES ('items.delete');
-INSERT INTO privileges (name) VALUES ('item_categories.update');
-INSERT INTO privileges (name) VALUES ('item_categories.insert');
-INSERT INTO privileges (name) VALUES ('item_categories.delete');
+INSERT INTO app_permissions (name) VALUES ('items.select');
+INSERT INTO app_permissions (name) VALUES ('items.update');
+INSERT INTO app_permissions (name) VALUES ('items.insert');
+INSERT INTO app_permissions (name) VALUES ('items.delete');
+INSERT INTO app_permissions (name) VALUES ('item_categories.select');
+INSERT INTO app_permissions (name) VALUES ('item_categories.update');
+INSERT INTO app_permissions (name) VALUES ('item_categories.insert');
+INSERT INTO app_permissions (name) VALUES ('item_categories.delete');
 
-INSERT INTO role_privileges(role_id, privilege_id) SELECT r.id as uid, p.id as pid FROM roles r, privileges p WHERE r.name = 'admin';
+INSERT INTO app_role_permissions(app_role_id, app_permission_id) SELECT r.id as uid, p.id as pid FROM app_roles r, app_permissions p WHERE r.name = 'admin';
 
-INSERT INTO user_roles(user_id, role_id) SELECT u.id as uid, r.id as rid FROM auth.users u, roles r WHERE u.email = 'agus@senomas.com' AND r.name = 'admin';
+INSERT INTO user_app_roles(user_id, app_role_id) SELECT u.id as uid, r.id as rid FROM auth.users u, app_roles r WHERE u.email = 'agus@senomas.com' AND r.name = 'admin';
--- sql/V2__create_table_item.sql
+++ sql/V2__create_table_item.sql
@@ -1,6 +1,10 @@
+DROP FUNCTION IF EXISTS fn_item_category_views CASCADE;
+DROP FUNCTION IF EXISTS fn_item_category_views CASCADE;
 DROP TABLE IF EXISTS item_categories CASCADE;
 DROP VIEW IF EXISTS item_category_views CASCADE;
+DROP FUNCTION IF EXISTS fn_item_views CASCADE;
 DROP TABLE IF EXISTS items CASCADE;
+DROP VIEW IF EXISTS item_views CASCADE;
 
 CREATE TABLE item_categories (
   id SERIAL NOT NULL PRIMARY KEY,
@@ -16,58 +20,83 @@ CREATE TABLE item_categories (
 
 ALTER TABLE item_categories ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY "select item_categories to all users" ON public.item_categories FOR SELECT USING (true);
-CREATE POLICY "update item_categories with privileges item_categories.update" ON public.item_categories FOR UPDATE USING (
-    EXISTS (SELECT 1 FROM user_roles ur INNER JOIN role_privileges rp ON ur.role_id = rp.role_id 
-      INNER JOIN privileges p ON rp.privilege_id = p.id 
+CREATE POLICY "select item_categories with permissions item_categories.select" ON public.item_categories FOR SELECT USING (
+    EXISTS (SELECT 1 FROM user_app_roles ur INNER JOIN app_role_permissions rp ON ur.app_role_id = rp.app_role_id 
+      INNER JOIN app_permissions p ON rp.app_permission_id = p.id 
+      WHERE p.name = 'item_categories.select' AND auth.uid() = ur.user_id)
+  );
+CREATE POLICY "update item_categories with permissions item_categories.update" ON public.item_categories FOR UPDATE USING (
+    EXISTS (SELECT 1 FROM user_app_roles ur INNER JOIN app_role_permissions rp ON ur.app_role_id = rp.app_role_id 
+      INNER JOIN app_permissions p ON rp.app_permission_id = p.id 
       WHERE p.name = 'item_categories.update' AND auth.uid() = ur.user_id)
   ) WITH CHECK (
-    EXISTS (SELECT 1 FROM user_roles ur INNER JOIN role_privileges rp ON ur.role_id = rp.role_id 
-      INNER JOIN privileges p ON rp.privilege_id = p.id 
+    EXISTS (SELECT 1 FROM user_app_roles ur INNER JOIN app_role_permissions rp ON ur.app_role_id = rp.app_role_id 
+      INNER JOIN app_permissions p ON rp.app_permission_id = p.id 
       WHERE p.name = 'item_categories.update' AND auth.uid() = ur.user_id)
   );
-CREATE POLICY "insert item_categories with privileges item_categories.insert" ON public.item_categories FOR INSERT WITH CHECK (
-    EXISTS (SELECT 1 FROM user_roles ur INNER JOIN role_privileges rp ON ur.role_id = rp.role_id 
-      INNER JOIN privileges p ON rp.privilege_id = p.id 
+CREATE POLICY "insert item_categories with permissions item_categories.insert" ON public.item_categories FOR INSERT WITH CHECK (
+    EXISTS (SELECT 1 FROM user_app_roles ur INNER JOIN app_role_permissions rp ON ur.app_role_id = rp.app_role_id 
+      INNER JOIN app_permissions p ON rp.app_permission_id = p.id 
       WHERE p.name = 'item_categories.insert' AND auth.uid() = ur.user_id)
   );
-CREATE POLICY "delete item_categories with privileges item_categories.delete" ON public.item_categories FOR DELETE USING (
-    EXISTS (SELECT 1 FROM user_roles ur INNER JOIN role_privileges rp ON ur.role_id = rp.role_id 
-      INNER JOIN privileges p ON rp.privilege_id = p.id 
+CREATE POLICY "delete item_categories with permissions item_categories.delete" ON public.item_categories FOR DELETE USING (
+    EXISTS (SELECT 1 FROM user_app_roles ur INNER JOIN app_role_permissions rp ON ur.app_role_id = rp.app_role_id 
+      INNER JOIN app_permissions p ON rp.app_permission_id = p.id 
       WHERE p.name = 'item_categories.delete' AND auth.uid() = ur.user_id)
   );
 
-CREATE RECURSIVE VIEW item_category_views (id, parent_id, parent, name, full_name, description, created_at, created_by, updated_at, updated_by) AS 
-SELECT
-	id,
-  parent_id,
-  '' AS parent,
-	name,
-  name AS full_name,
-  description,
-  created_at,
-  created_by,
-  updated_at,
-  updated_by
-FROM
-	item_categories
-WHERE
-	parent_id IS NULL
-UNION ALL
-	SELECT
-		e.id,
-    e.parent_id,
-    rl.name AS parent,
-    e.name,
-    rl.name || ' || ' || e.name AS full_name,
-    e.description,
-    e.created_at,
-    e.created_by,
-    e.updated_at,
-    e.updated_by
-	FROM
-		item_categories e
-	INNER JOIN item_category_views rl ON e.parent_id = rl.id;
+
+CREATE FUNCTION fn_item_category_views()
+RETURNS TABLE (
+  id INT,
+  parent_id INT,
+  parent TEXT,
+	name TEXT,
+  full_name TEXT,
+  description TEXT,
+  created_at TIMESTAMP WITH TIME ZONE,
+  created_by UUID,
+  updated_at TIMESTAMP WITH TIME ZONE,
+  updated_by UUID
+)
+LANGUAGE sql
+SECURITY INVOKER
+AS $$
+  WITH RECURSIVE item_category_recursive (id, parent_id, parent, name, full_name, description, created_at, created_by, updated_at, updated_by) AS (
+  SELECT
+    id,
+    parent_id,
+    '' AS parent,
+    name,
+    name AS full_name,
+    description,
+    created_at,
+    created_by,
+    updated_at,
+    updated_by
+  FROM
+    item_categories
+  WHERE
+    parent_id IS NULL
+  UNION ALL
+    SELECT
+      e.id,
+      e.parent_id,
+      rl.name AS parent,
+      e.name,
+      rl.name || ' || ' || e.name AS full_name,
+      e.description,
+      e.created_at,
+      e.created_by,
+      e.updated_at,
+      e.updated_by
+    FROM
+      item_categories e
+    INNER JOIN item_category_recursive rl ON e.parent_id = rl.id) SELECT * FROM item_category_recursive;
+$$;
+
+CREATE VIEW item_category_views AS SELECT * FROM fn_item_category_views();
+
 
 CREATE TABLE items (
   id SERIAL NOT NULL PRIMARY KEY,
@@ -82,31 +111,53 @@ CREATE TABLE items (
 );
 
 ALTER TABLE items ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "select items to all users" ON public.items FOR SELECT USING (true);
-CREATE POLICY "update items with privileges items.update" ON public.items FOR UPDATE USING (
-    EXISTS (SELECT 1 FROM user_roles ur INNER JOIN role_privileges rp ON ur.role_id = rp.role_id 
-      INNER JOIN privileges p ON rp.privilege_id = p.id 
+CREATE POLICY "select items with permissions items.select" ON public.items FOR SELECT USING (
+    EXISTS (SELECT 1 FROM user_app_roles ur INNER JOIN app_role_permissions rp ON ur.app_role_id = rp.app_role_id 
+      INNER JOIN app_permissions p ON rp.app_permission_id = p.id 
+      WHERE p.name = 'items.select' AND auth.uid() = ur.user_id)
+  );
+CREATE POLICY "update items with permissions items.update" ON public.items FOR UPDATE USING (
+    EXISTS (SELECT 1 FROM user_app_roles ur INNER JOIN app_role_permissions rp ON ur.app_role_id = rp.app_role_id 
+      INNER JOIN app_permissions p ON rp.app_permission_id = p.id 
       WHERE p.name = 'items.update' AND auth.uid() = ur.user_id)
   ) WITH CHECK (
-    EXISTS (SELECT 1 FROM user_roles ur INNER JOIN role_privileges rp ON ur.role_id = rp.role_id 
-      INNER JOIN privileges p ON rp.privilege_id = p.id 
+    EXISTS (SELECT 1 FROM user_app_roles ur INNER JOIN app_role_permissions rp ON ur.app_role_id = rp.app_role_id 
+      INNER JOIN app_permissions p ON rp.app_permission_id = p.id 
       WHERE p.name = 'items.update' AND auth.uid() = ur.user_id)
   );
-CREATE POLICY "insert items with privileges items.insert" ON public.items FOR INSERT WITH CHECK (
-    EXISTS (SELECT 1 FROM user_roles ur INNER JOIN role_privileges rp ON ur.role_id = rp.role_id 
-      INNER JOIN privileges p ON rp.privilege_id = p.id 
+CREATE POLICY "insert items with permissions items.insert" ON public.items FOR INSERT WITH CHECK (
+    EXISTS (SELECT 1 FROM user_app_roles ur INNER JOIN app_role_permissions rp ON ur.app_role_id = rp.app_role_id 
+      INNER JOIN app_permissions p ON rp.app_permission_id = p.id 
       WHERE p.name = 'items.insert' AND auth.uid() = ur.user_id)
   );
-CREATE POLICY "delete items with privileges items.delete" ON public.items FOR DELETE USING (
-    EXISTS (SELECT 1 FROM user_roles ur INNER JOIN role_privileges rp ON ur.role_id = rp.role_id 
-      INNER JOIN privileges p ON rp.privilege_id = p.id 
+CREATE POLICY "delete items with permissions items.delete" ON public.items FOR DELETE USING (
+    EXISTS (SELECT 1 FROM user_app_roles ur INNER JOIN app_role_permissions rp ON ur.app_role_id = rp.app_role_id 
+      INNER JOIN app_permissions p ON rp.app_permission_id = p.id 
       WHERE p.name = 'items.delete' AND auth.uid() = ur.user_id)
   );
 
-CREATE VIEW item_views AS
-SELECT
-  i.*,
-  ic.full_name AS category
-FROM
-  items i 
-  INNER JOIN item_category_views ic ON i.category_id = ic.id;
+
+CREATE FUNCTION fn_item_views()
+RETURNS TABLE (
+  id INT,
+  created_at TIMESTAMP WITH TIME ZONE,
+  created_by UUID,
+  updated_at TIMESTAMP WITH TIME ZONE,
+  updated_by UUID,
+  category_id INT,
+  name TEXT,
+  description TEXT,
+  category TEXT
+)
+LANGUAGE sql
+SECURITY INVOKER
+AS $$
+  SELECT
+    i.*,
+    ic.full_name AS category
+  FROM
+    items i 
+    INNER JOIN item_category_views ic ON i.category_id = ic.id;
+$$;
+
+CREATE VIEW item_views AS SELECT * FROM fn_item_views();
