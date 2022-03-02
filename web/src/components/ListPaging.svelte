2fbfa6a270b511e22e9cae7793280eff0e475219
denmaseno <agus@hanoman.co.id>
2022-03-03 06:47:18 +0700

    fix pagination

--- web/src/components/ListPaging.svelte
+++ web/src/components/ListPaging.svelte
@@ -1,73 +1,37 @@
 <script lang="ts">
+  import {goto} from '$app/navigation';
+
   export let value;
 
-  const pages = [];
-  const pageMax = Math.floor(
-    (value.total + value.paging.pageSize - 1) / value.paging.pageSize
-  );
-  const pagingSize1 = value.paging.pageSize - 1;
-  const pagingSize2 = Math.floor(value.paging.pageSize / 2);
-  let pmin = Math.max(1, value.paging.page - pagingSize2);
-  const pmax = Math.min(pageMax, pmin + pagingSize1);
-  pmin = Math.max(1, pmax - pagingSize1);
-
-  if (value.paging.page > 1) {
-    pages.push({
-      title: '\u2759\u276E',
-      link: value.createLink({
-        paging: {...value.paging, page: 1}
-      })
-    });
-    pages.push({
-      title: '\u276E\u276E',
-      link: value.createLink({
-        paging: {...value.paging, page: Math.max(1, pmin - 1)}
-      })
-    });
-    pages.push({
-      title: '\u276E',
-      link: value.createLink({
-        paging: {...value.paging, page: value.paging.page - 1}
-      })
-    });
-  } else {
-    pages.push({title: '\u2759\u276E'});
-    pages.push({title: '\u276E\u276E'});
-    pages.push({title: '\u276E'});
-  }
-  for (let p = pmin; p <= pmax; p++) {
-    if (p === value.paging.page) {
-      pages.push({title: String(p)});
-    } else {
-      pages.push({
-        title: String(p),
-        link: value.createLink({paging: {...value.paging, page: p}})
-      });
+  let pageMax;
+  let pmin;
+  let pmax;
+  let pages = [];
+  $: {
+    pageMax = Math.floor(
+      (value.total + value.paging.pageSize - 1) / value.paging.pageSize
+    );
+    const pagingSize1 = value.paging.pageSize - 1;
+    const pagingSize2 = Math.floor(value.paging.pageSize / 2);
+    pmin = Math.max(1, value.paging.page - pagingSize2);
+    pmax = Math.min(pageMax, pmin + pagingSize1);
+    pmin = Math.max(1, pmax - pagingSize1);
+    pages = [];
+    for (let p = pmin; p <= pmax; p++) {
+      pages.push(p);
     }
   }
-  if (value.paging.page < pageMax) {
-    pages.push({
-      title: '\u276F',
-      link: value.createLink({
-        paging: {...value.paging, page: value.paging.page + 1}
-      })
-    });
-    pages.push({
-      title: '\u276F\u276F',
-      link: value.createLink({
-        paging: {...value.paging, page: Math.min(pageMax, pmax + 1)}
-      })
-    });
-    pages.push({
-      title: '\u276F\u2759',
-      link: value.createLink({
-        paging: {...value.paging, page: pageMax}
-      })
-    });
-  } else {
-    pages.push({title: '\u276F'});
-    pages.push({title: '\u276F\u276F'});
-    pages.push({title: '\u276F\u2759'});
+
+  function goPage(p) {
+    return () => {
+      if (p <= 1) {
+        goto(value.createLink({paging: {...value.paging, page: 1}}));
+      } else if (p >= pageMax) {
+        goto(value.createLink({paging: {...value.paging, page: pageMax}}));
+      } else {
+        goto(value.createLink({paging: {...value.paging, page: p}}));
+      }
+    };
   }
 </script>
 
@@ -81,14 +45,37 @@
     {/if}
   </div>
   <div class="right">
+    <span
+      class="px-2 py-2 {value.paging.page > 1 ? 'link' : ''}"
+      on:click={goPage(1)}>&#x2759;&#x276E;</span
+    >
+    <span
+      class="px-2 py-2 {value.paging.page > 1 ? 'link' : ''}"
+      on:click={goPage(pmin - 1)}>&#x276E;&#x276E;</span
+    >
+    <span
+      class="px-2 py-2 {value.paging.page > 1 ? 'link' : ''}"
+      on:click={goPage(value.paging.page - 1)}>&#x276E;</span
+    >
+
     {#each pages as p}
-      {#if p.link}
-        <a class="px-2 py-2 text-blue-700 cursor-pointer" href={p.link}
-          >{p.title}</a
-        >
-      {:else}
-        <span class="px-2 py-2">{p.title}</span>
-      {/if}
+      <span
+        class="px-2 py-2 {value.paging.page !== p ? 'link' : ''}"
+        on:click={goPage(p)}>{p}</span
+      >
     {/each}
+
+    <span
+      class="px-2 py-2 {value.paging.page < pageMax ? 'link' : ''}"
+      on:click={goPage(value.paging.page + 1)}>&#x276F;</span
+    >
+    <span
+      class="px-2 py-2 {value.paging.page < pageMax ? 'link' : ''}"
+      on:click={goPage(pmax + 1)}>&#x276F;&#x276F;</span
+    >
+    <span
+      class="px-2 py-2 {value.paging.page < pageMax ? 'link' : ''}"
+      on:click={goPage(pageMax)}>&#x276F;&#x2759;</span
+    >
   </div>
 </div>
