b2fec8359859767e542ddd8ae66a979c9c157b7e
denmaseno <agus@hanoman.co.id>
2022-03-02 17:51:35 +0700

    fix combo box logic

--- web/src/components/Input.svelte
+++ web/src/components/Input.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import {onMount} from 'svelte';
+
   export let id;
   export let tagId = id;
   export let label = tagId;
@@ -11,6 +13,7 @@
   let focus = false;
   let listOpen = false;
   let selectValue;
+  let selectValueLC;
 
   function select(opt, _) {
     value[id] = opt?.key;
@@ -19,18 +22,28 @@
   function onFocus() {
     focus = true;
     listOpen = true;
+    if (type === 'select' && options) {
+      selectValue = '';
+    }
   }
 
   function onBlur() {
     focus = false;
     listOpen = false;
+    if (type === 'select' && options) {
+      selectValue = options.filter(({key}) => key === value[id])[0]?.value;
+    }
   }
 
-  $: {
+  onMount(() => {
     selectValue =
       type === 'select' && options
         ? options.filter(({key}) => key === value[id])[0]?.value
         : '';
+  });
+
+  $: {
+    selectValueLC = selectValue?.toLowerCase();
   }
 </script>
 
@@ -77,29 +90,45 @@
       : 'top-[2rem]'} transition-all"
     for={id}>{label}</label
   >
-  {#if type === 'select' && listOpen}
+  {#if type === 'select'}
     <div
-      class="border border-gray-500 rounded bg-white absolute top-14 z-50 w-full"
+      class="pointer-events-none absolute top-7 inset-y-0 right-0 flex items-center text-gray-700"
     >
-      <ul>
-        <li
-          class="hover:bg-gray-300"
-          on:mousedown|stopPropagation={e => select(null, e)}
-        >
-          <span class="px-2" />
-        </li>
-        {#if options}
-          {#each options as opt}
-            <li
-              class="hover:bg-gray-300"
-              on:mousedown|stopPropagation={e => select(opt, e)}
-            >
-              <span class="px-2">{opt.value}</span>
-            </li>
-          {/each}
-        {/if}
-      </ul>
+      <svg
+        class="fill-current h-4 w-4"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        ><path
+          d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+        /></svg
+      >
     </div>
+    {#if listOpen}
+      <div
+        class="border border-gray-500 rounded bg-white absolute top-14 z-50 w-full max-h-[15rem] overflow-y-auto"
+      >
+        <ul>
+          <li
+            class="hover:bg-gray-300"
+            on:mousedown|stopPropagation={e => select(null, e)}
+          >
+            <span class="px-2" />
+          </li>
+          {#if options}
+            {#each options as opt}
+              {#if opt?.value?.toLowerCase().includes(selectValueLC)}
+                <li
+                  class="hover:bg-gray-300"
+                  on:mousedown|stopPropagation={e => select(opt, e)}
+                >
+                  <span class="px-2">{opt.value}</span>
+                </li>
+              {/if}
+            {/each}
+          {/if}
+        </ul>
+      </div>
+    {/if}
   {/if}
   {#if validate[id]}
     <p class="text-red-500 text-xs italic">{validate[id]}</p>
