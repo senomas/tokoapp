a5a8bd69d48eacdd352dcdd9e78ba74de5478dc7
denmaseno <agus@hanoman.co.id>
2022-03-10 14:44:01 +0700

    put yaml file in static folder

--- web/src/routes/[...file].svelte
+++ web/src/routes/[...file].svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import {page as pageStore} from '$app/stores';
+  import {load} from 'js-yaml';
 
   let data;
   let component;
@@ -8,17 +9,37 @@
     (async () => {
       let data;
       let component;
+      let config;
       const pathname = $pageStore.url.pathname;
-      const res = await fetch(
-        `/data${pathname === '' || pathname === '/' ? '/index' : pathname}.json`
-      );
-      if (res.status === 200) {
-        data = await res.json();
+      if (pathname.endsWith('/')) {
+        config = pathname + 'index.yaml';
+      } else {
+        config = pathname + '.yaml';
+      }
+      console.log({config});
+      let res = await fetch(config, {headers: {Accept: 'text/yaml'}});
+      if (
+        res.status === 200 &&
+        res.headers.get('content-type') === 'text/yaml'
+      ) {
+        const text = await res.text();
+        data = load(text);
         component = (await import(`../components/${data.__type}.svelte`))
           .default;
         delete data.__type;
       } else {
-        console.log({res});
+        config = pathname + '/index.yaml';
+        res = await fetch(config, {headers: {Accept: 'text/yaml'}});
+        if (
+          res.status === 200 &&
+          res.headers.get('content-type') === 'text/yaml'
+        ) {
+          const text = await res.text();
+          data = load(text);
+          component = (await import(`../components/${data.__type}.svelte`))
+            .default;
+          delete data.__type;
+        }
       }
       return {data, component};
     })().then(v => {
@@ -29,5 +50,7 @@
 </script>
 
 {#if component}
-  <svelte:component this={component} {...data} />
+  {#key component}
+    <svelte:component this={component} {...data} />
+  {/key}
 {/if}
